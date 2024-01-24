dfc5a0c695cab7d86eaa84f2512486fd684fada8
Matt Ferraro <mferraro89@gmail.com>
2024-01-24 14:32:23 -0500

    struggling to simulate an impulse

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,19 @@ fn main() {
     let mut w: Array2<f64> = Array2::zeros((height, width));
 
     // v[[height / 2, width / 5]] = 100.0;
-    v[[height / 2, 10]] = 100.0;
+    // v[[height / 2, 10]] = 100.0;
+    let source_y = 60;
+    let source_x = 70;
+    // u[[source_y, source_x]] = 100.0;
+    v[[source_y, source_x]] = 100.0;
+    // w[[source_y, source_x]] = 100.0;
+    // v[[source_y + 1, source_x]] = 100.0;
+    // v[[source_y - 1, source_x]] = 100.0;
+    // v[[source_y, source_x + 1]] = 100.0;
+    // v[[source_y, source_x - 1]] = 100.0;
 
-    let probe_x = height / 2;
-    let probe_y = 20;
+    let probe_x = 60;
+    let probe_y = 80;
 
     // so distance between source and sensor is 150 - 10 - 10 = 130 decimeters = 13 meters
     // sound travels at 340 m/s so covering 13 meters should take 38.24 ms.
@@ -33,25 +42,55 @@ fn main() {
 
     let mut step_count = 0;
 
-    for i in 0..15 {
+    for i in 0..30 {
         // 44300 / 60 = 738 so to simulate a frame for a 60 fps video, step 738 times
         // but remember we use a triple step, so just 246 steps is enough
         // for a 60 FPS movie that plays at 1/4 real speed, use 246 / 4 = 62
-        for _j in 0..62 {
-            let (a, b, c) = three_step(&mut u, &mut v, &mut w, probe_x, probe_y);
-            step_count += 3;
+        for _j in 0..1 {
+            // let (a, b, c) = three_step(&mut u, &mut v, &mut w, probe_x, probe_y);
 
-            // if a > 0.001 {
-            //     println!("Disturbance at step {}", step_count);
-            //     return;
-            // }
+            println!("Saving frame: {}", step_count);
+            save_img(&v, step_count);
+
+            step(&mut u, &mut v, &mut w);
+            let a = w[[probe_x, probe_y]];
             samples.push(a as f32);
+            step_count += 1;
+
+            if step_count == 1 {
+                w[[source_y, source_x]] = 0.0;
+            }
+
+            println!("Saving frame: {}", step_count);
+            save_img(&w, step_count);
+
+            step(&mut v, &mut w, &mut u);
+            let b = u[[probe_x, probe_y]];
             samples.push(b as f32);
+            step_count += 1;
+
+            if step_count == 2 {
+                u[[source_y, source_x]] = 0.0;
+            }
+
+            println!("Saving frame: {}", step_count);
+            save_img(&u, step_count);
+
+            step(&mut w, &mut u, &mut v);
+            let c = v[[probe_x, probe_y]];
             samples.push(c as f32);
+            step_count += 1;
+
+            if step_count == 3 {
+                v[[source_y, source_x]] = 0.0;
+            }
+
+            println!("Saving frame: {}", step_count);
+            save_img(&v, step_count);
         }
 
-        println!("Saving frame: {}", i);
-        save_img(&v, i)
+        // println!("Saving frame: {}", i);
+        // save_img(&v, i)
     }
     println!("Total steps: {}", step_count);
 
@@ -62,7 +101,7 @@ fn main() {
     // wav::write(header, &data, &mut out_file);
 }
 
-fn not_main() -> Result<(), std::io::Error> {
+fn _not_main() -> Result<(), std::io::Error> {
     // let mut inp_file = File::open(Path::new("hello.wav"))?;
     // let (header, data) = wav::read(&mut inp_file)?;
     let h = wav::Header {
@@ -113,6 +152,10 @@ fn save_as_wave(signal: Vec<f32>, filename: &str) -> Result<(), std::io::Error> 
     for sample in signal.iter() {
         if *sample > max {
             max = *sample
+        }
+
+        if *sample * -1.0 > max {
+            max = *sample * -1.0;
         }
     }
 
@@ -177,14 +220,37 @@ fn array_to_image(arr: Array3<u8>) -> RgbImage {
         .expect("container should have the right size for the image dimensions")
 }
 
-fn step(u: &mut Array2<f64>, v: &mut Array2<f64>, w: &mut Array2<f64>) {
+fn step(a: &mut Array2<f64>, b: &mut Array2<f64>, c: &mut Array2<f64>) {
+    let (height, width) = a.dim();
+    // u is the array at t - 1  (think a)
+    // v is the array at t      (think b)
+    // w is the array at t + 1  (think c)
+    let v_sound: f64 = 340.0; // m/s
+    let delta_t: f64 = 1.0 / 44300.0; // samples/sec
+    let delta_x: f64 = 0.1; // meters
+    let delta_x_sq = delta_x * delta_x;
+    let cfl_const: f64 = delta_t * delta_t * v_sound * v_sound;
+    // println!("cfl: {}", cfl_const);
+    for i in 1..width - 1 {
+        for j in 1..height - 1 {
+            // i goes from 0..width so is the x axis
+            // j goes from 0..height so is the y axis
+
+            let dxdxu = (b[[j, i - 1]] + b[[j, i + 1]] - 2.0 * b[[j, i]]) / delta_x_sq;
+            let dydyu = (b[[j - 1, i]] + b[[j + 1, i]] - 2.0 * b[[j, i]]) / delta_x_sq;
+            c[[j, i]] = 2.0 * b[[j, i]] - a[[j, i]] + cfl_const * (dxdxu + dydyu);
+        }
+    }
+}
+
+fn _step_old(u: &mut Array2<f64>, v: &mut Array2<f64>, w: &mut Array2<f64>) {
     let (height, width) = u.dim();
 
     // println!("In step: w: {} h: {}", width, height);
     // u is the array at t - 1
     // v is the array at t
     // w is the array at t + 1
-    let c = 0.0049;
+    let c = 0.15;
     // for ((i, j), val) in u.indexed_iter() {
     for i in 1..width - 1 {
         for j in 1..height - 1 {
